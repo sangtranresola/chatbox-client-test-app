61c3fab75d05a3a47deb4c4a68cdae6a857e2ab3
SangTran <112836164+sangtranresola@users.noreply.github.com>
2024-05-31 08:42:46 +0700

    fix scripts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import './App.css'
+import { DateTime } from 'luxon';
 
 function App() {
+  function getCurrentDate(dateTime: string | Date, format: string) {
+    if (!dateTime) return '';
+    const dateTest = dateTime instanceof Date ? dateTime.toISOString() : dateTime;
+    const dateTimeFormat = format || 'Y年m月d日 H:i:s';
+  
+    if (dateTime === 'now')
+      return DateTime.now().toFormat(dateTimeFormat);
+    
+    return DateTime.fromISO(dateTest).toFormat(dateTimeFormat);
+  }
 
   return (
     <>
