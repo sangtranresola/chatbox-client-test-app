95b3232772e0e2af13960e46db8a7c95e2256f92
SangTran <112836164+sangtranresola@users.noreply.github.com>
2024-07-03 15:19:05 +0700

    add local script

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,46 @@ function App() {
     
     return DateTime.fromISO(dateTest).toFormat(dateTimeFormat);
   }
-
+  const markdown2 = `<h1>Hi</h1> <h3>Pluto</h3>! **Welcome**  to <br/> the <br/> <h2 class="code-line" data-line-start=14 data-line-end=15 ><a id="Features_14"></a>Features</h2><ul>
+  <img src="https://picsum.photos/200/300" alt="placeholder" />
+  </ul>`
   return (
     <>
-      <h1>Chatbox Client Demo</h1>
+      <h1 className="text-3xl font-bold underline border-red italic hover:not-italic">
+        Hello world!
+      </h1>
+      <ul className="list">
+					<li className="list-item">
+						<div className="item2">
+							<p className="index2">
+                <img src="../images/tel2.png" /><a href="tel:0120-000-000">0120-000-000</a></p>
+							<p className="note">※携帯電話・スマートフォンからもご利用になれます。</p>
+						</div>
+					</li>
+					<li className="list-item">
+						<div className="item2">
+							<p className="comment">example@example.jp</p>
+							<p className="link">お問い合わせメールは<a href="mailto:example@example.jp">こちら</a>をクリック</p>
+						</div>
+					</li>
+					<li className="list-item">
+						<div className="item2">
+							<p className="link">お問い合わせフォームは<a href="https://sienplatz.form.kintoneapp.com/public/8c6b48ea691e3f2a8067543fb70ed82bcc92d2e46d234d301d5fee4f75fed838" target="_blank">こちら</a>をクリック</p>
+						</div>
+					</li>
+					<li className="list-item">
+						<div className="item1 remote">
+							<p className="index">リモートサポート</p>
+							<p className="remote-img"><img src="../images/remote002.png" /></p>
+						</div>
+					</li>
+				</ul>
+      <span
+          className="font-base"
+          dangerouslySetInnerHTML={{
+            __html: markdown2,
+          }}
+        ></span>
     </>
   )
 }
