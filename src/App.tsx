fb6d734bb301b4ee039d32c87e72a759fba31549
SangTran <112836164+sangtranresola@users.noreply.github.com>
2024-09-30 09:56:23 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -65,15 +65,14 @@ function App() {
   // }
 
 
-  const onToggleChatWindow1 = () => {
+  const onToggleChatWindowDEVCAT = () => {
     // const thisChatId = '01hwmc2d7fyenbb0cbxdf66f2x'
     // const thisChat = window[`__DECA_CLIENT__${thisChatId}`]
     // console.log('toggle chatwindow1', thisChat);
     // if (thisChat) {
     //   thisChat.chat.toggleChatWindow(thisChatId);
     // }
-   window.__DECA_CLIENT__01hxapnpyz2ah0q21ptpm3z7kj.chat.toggleChatWindow('01hxapnpyz2ah0q21ptpm3z7kj')
-    // window.__DECA_CLIENT__01hwmc2d7fyenbb0cbxdf66f2x.chat.toggleChatWindow('01hwmc2d7fyenbb0cbxdf66f2x');
+  //  window.__DECA_CLIENT__01hz4dpn25es3kmvahskfzze9e.chat.toggleChatWindow('01hz4dpn25es3kmvahskfzze9e')
   };
 
   // onclick="window.__DECA_CLIENT__01hwmc2d7fyenbb0cbxdf66f2x.chat.toggleChatWindow('01hwmc2d7fyenbb0cbxdf66f2x')"
@@ -86,18 +85,18 @@ function App() {
     //   console.log('run chatwindow2', thisChat);
     //   thisChat.chat.toggleChatWindow(thisChatId);
     // }
-    window.__DECA_CLIENT__01j69k1ddvyx35xyc9zj1j19s8.chat.toggleChatWindow('01j69k1ddvyx35xyc9zj1j19s8');
+    // window.__DECA_CLIENT__01j69k1ddvyx35xyc9zj1j19s8.chat.toggleChatWindow('01j69k1ddvyx35xyc9zj1j19s8');
     // window.__DECA_CLIENT__01hz4dpn25es3kmvahskfzze9e.chat.toggleChatWindow('01hz4dpn25es3kmvahskfzze9e');
   };
 
   return (
     <>
-      <div onClick={onToggleChatWindow1}>
-        ToggleChatbox-1-yellow
+      <div onClick={onToggleChatWindowDEVCAT}>
+        ToggleChatbox
       </div>
-      <div onClick={onToggleChatWindow2}>
+      {/* <div onClick={onToggleChatWindow2}>
         ToggleChatbox-2-black
-      </div>
+      </div> */}
       <h1 className="text-3xl font-bold underline border-red italic hover:not-italic">
         Hello world!
       </h1>
